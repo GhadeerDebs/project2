bb5a3aef218b4aa3eda6a99dadf515ae190d4f06
Hala7C <halasaadeh04@gmail.com>
2022-08-17 09:40:08 +0300

    api show

--- car_proj/app/Http/Controllers/API/AdvetiseController.php
+++ car_proj/app/Http/Controllers/API/AdvetiseController.php
@@ -66,7 +66,8 @@ if($a){
         $model=$a->model()->get('name');
         $year=$a->year();
         $make=$a->make();
-        $dealer=$a->dealership()->get('name');
+        $dealername=$a->dealership()->get('name');
+        $dealer=$a->dealership()->get('phone');
        // $pictures[]=$a->pictures()->get();
    //   $pictures=$a->pictures()->get();
         $data->push([
@@ -81,7 +82,7 @@ if($a){
             'created_at'=>$a->created_at,
             'updated_at'=>$a->updated_at,
             'dealership_id'=>$a->dealership_id,
-            'dealership_name'=>$dealer[0]['name'],
+            'dealership_name'=>$dealername[0]['name'],
             'dealership_phone'=>$dealer[0]['phone'],
             'equipment' => $a->equipment,
             'entertainment_equipment' =>$a->entertainment_equipment,
